8d9095d9e2b4942474b49874a110e80c34f6d1b9
Nargiza Salomova <nargizusya@gmail.com>
2024-09-08 21:53:51 +0500

    Стилизует секцию hero в мобильной версии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,29 +3,13 @@ const menuToggle = document.querySelector('.main-header__toggle');
 const navMenu = document.querySelector('.main-nav');
 
 menuToggle.addEventListener('click',() =>{
-  navMenu.classList.toggle('main-nav--closed');
   if(menuToggle.classList.contains('main-header__toggle--closed')){
     menuToggle.classList.remove('main-header__toggle--closed');
     menuToggle.classList.add('main-header__toggle--opened');
+    navMenu.classList.remove('main-nav--closed');
   } else{
     menuToggle.classList.remove('main-header__toggle--opened');
     menuToggle.classList.add('main-header__toggle--closed');
+    navMenu.classList.add('main-nav--closed');
   }
 });
-
-// if(menuToggle.classList.contains('main-header__toggle--opened')){
-//   menuToggle.addEventListener('click', () =>{
-//     menuToggle.classList.remove('main-header__toggle--opened');
-//     menuToggle.classList.add('main-header__toggle--closed');
-//     navMenu.classList.add('main-nav--closed');
-//   });
-// }
-
-// if(menuToggle.classList.contains('main-header__toggle--closed')){
-//   menuToggle.addEventListener('click', () =>{
-//     menuToggle.classList.remove('main-header__toggle--closed');
-//     menuToggle.classList.add('main-header__toggle--opened');
-//     navMenu.classList.remove('main-nav--closed');
-//   });
-// }
-
