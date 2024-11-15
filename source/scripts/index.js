4754b7b82b316485b87b10a52e4915f4a8297ea8
Nargiza <127876836+NargizaSalomova@users.noreply.github.com>
2024-11-15 20:39:19 +0500

    Мелкая сетка (#5)
    * стилизует шапку в мобильной версии
    
    * стилизует шапку планшетной версии
    
    * Стилизует шапку в десктопной версии
    
    * Добавляет к бургер меню script
    
    * Стилизует секцию hero в мобильной версии
    
    * Стилизует секцию hero в планшетной версии
    
    * Стилизует секцию hero в десктопной версии
    
    * Стилизует секцию diet мобильной версии
    
    * Стилизует секцию diet планшетной версии
    
    * Стилизует секцию diet десктопной версии
    
    * Стилизует секцию description мобильной версии
    
    * Добавляет breakpoint 1024px
    
    * Стилизация секции description
    
    * Стилизует grid сетку секции result
    
    * Стилизует слайдер мобильной версии
    
    * Добавляет script в слайдер
    
    * Стилизует background в tablet версии
    
    * Стилизует секцию result на ширине экрана 1024px
    
    * Стилизует секцию result для больших экранов
    
    * Стилизует карту и footer
    
    * Исправляет ошибку по lint
    
    * Добавляет iframe для карты
    
    * Добавляет nojs в header и стилизует котика в hero
    
    * Заканчивает стилизацию index
    
    * Добавляет стилизованный header  и карту в catalog и forms
    
    * Стилизует catalog mobile
    
    * Исправляет по ПП
    
    * Исправляет по lint
    
    * Исправляет проект по замечаниям наставника
    
    * Удаляет alt из source

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,25 @@
 /* в этот файл добавляет скрипты*/
+const mainHeader = document.querySelector('.main-header');
+const menuToggle = document.querySelector('.main-header__toggle');
+const navMenu = document.querySelector('.main-nav');
+
+mainHeader.classList.remove('main-header--nojs');
+menuToggle.removeAttribute('disabled');
+
+menuToggle.addEventListener('click',() =>{
+  if(menuToggle.classList.contains('main-header__toggle--closed')){
+    menuToggle.classList.remove('main-header__toggle--closed');
+    menuToggle.classList.add('main-header__toggle--opened');
+    navMenu.classList.remove('main-nav--closed');
+  } else{
+    menuToggle.classList.remove('main-header__toggle--opened');
+    menuToggle.classList.add('main-header__toggle--closed');
+    navMenu.classList.add('main-nav--closed');
+  }
+});
+
+const imagesCat = document.querySelector('.images-cat');
+
+document.querySelector('.images-cat__slider').addEventListener('input', (e)=>{
+  imagesCat.style.setProperty('--position', `${e.target.value}%`);
+});
